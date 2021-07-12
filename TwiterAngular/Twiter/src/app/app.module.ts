c53d4bc700454536aa0b26e05d9858c452bcac50
phuong98-sys <phuong98mta@gmail.com>
2021-07-12 18:07:48 +0700

    12/7

--- TwiterAngular/Twiter/src/app/app.module.ts
+++ TwiterAngular/Twiter/src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedComponent } from './shared/shared.component';
-import { LayoutComponent } from './layout/layout.component';
-import { MainComponent } from './main/main.component';
+import { LeftMenuComponent } from './main/shared/left-menu/left-menu.component';
+import { TrendsComponent } from './main/layout/trends/trends.component';
+import { HeaderComponent } from './main/layout/header/header.component';
+import { HomeComponent } from './main/content/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SharedComponent,
-    LayoutComponent,
-    MainComponent
+    LeftMenuComponent,
+    TrendsComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
